b308f50258f734ac5cb8b27b1d1446a7b9d171bf
Xynix91 <hprasetya02@gmail.com>
2018-09-26 18:41:56 -0700

    Update test.js

--- test.js
+++ test.js
@@ -203,7 +203,7 @@ var graph = function(func, vars, constraints, x, y, w, h) {
 //println('f(2) = ' + evaluate(f, ['x'], [2]));
 
 //graph(f, ['x'], [[-2, 2], [-4, 4]], 100, 100);
-void draw() {
+//void draw() {
     //background(255, 255, 255);
     //graph(f, ['x'], [[-2, 2], [-4, 4]]);
-};
+//};
