b57aeff07965224f4a234d0b89ce18813501a3ae
Xynix91 <hprasetya02@gmail.com>
2018-10-13 21:08:53 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -103,8 +103,8 @@ var operate = function(func, idx, combine) {
         parseFloat(func.substring(second[0], second[1]))
     );
     
-    return {'num': total.toString(), 'newPos': idx - (first[1] - first[0]) + total.toString().length - 1, 'start': first[0], 'end': second[1]};
-};
+    var res = {'num': total.toString(), 'newPos': idx - (first[1] - first[0]) + total.toString().length - 1, 'start': first[0], 'end': second[1]};
+    return res;
 
 var add = function(a, b) { return a + b; };
 var subtract = function(a, b) { return a - b; };
