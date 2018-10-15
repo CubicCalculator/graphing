fa9fe37ac52303458807b05259eecac75644f4dc
CaboozledPie <43527500+CaboozledPie@users.noreply.github.com>
2018-10-14 22:29:19 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -787,10 +787,10 @@ var editconstraints = function(func, old) {
     stroke(0, 0, 0);
     strokeWeight(2);
     line(width/2-(min(width, height) -200)/2, height/2-(min(width, height) -200)/2.5, width/2+(min(width, height) -200)/2, height/2-(min(width, height) -200)/2.5);
-    textAlign(LEFT, BASELINE);
     textSize(15*min(width,height)/600);
-    text("Minimum x-value:", width/2-(min(width, height) -200)*3/8, 150+(min(width, height)-200)/4);
-    text("Maximum x-value:", width/2+(min(width, height) -200)*3/8, 150+(min(width, height)-200)/4);
+    text("Minimum x-value:", width/2-((min(width, height) -200)*3/8), 150+(min(width, height)-200)/4);
+    text("Maximum x-value:", width/2+((min(width, height) -200)*3/8), 150+(min(width, height)-200)/4);
+    textAlign(LEFT, BASELINE);
     constraintedit(min(width, height)/2-160, min(width,height)/2-100, str(func.constraints[0][0]), "startX", func, [0, 0]);
     constraintedit(min(width, height)/2+60, min(width,height)/2-100, str(func.constraints[0][1]), "endX", func, [0, 1]);
     if (func.constraints.length === 2) {
