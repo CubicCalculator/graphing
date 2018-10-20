23a271cd2734b5e0af543a64184baac8d22272d8
CaboozledPie <43527500+CaboozledPie@users.noreply.github.com>
2018-10-19 22:48:35 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2314,17 +2314,17 @@ void draw() {
             constraintPos = str(prevGraph.constraints[0][0]).length;
             currCons = "startX";
         }
-        if (button((i%floor(width/200))*min(width, height)/1.5 + min(width,height)/4-10, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+457+funcTranslate, 20, 20, 2, 226, 227, 245)&&editCon === false&&helpopen === false) {
+        if (button((i%floor(width/200))*min(width, height)/1.5 + min(width,height)/4+60, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+457+funcTranslate, 20, 20, 2, 226, 227, 245)&&editCon === false&&helpopen === false) {
             grapphs.splice(i, 1);
             break;
         }
         stroke(255, 0, 0);
         strokeWeight(3);
-        line(i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+60, (min(width, height)/1.5+30)*(floor(i/floor(width/200)))+472,i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+70, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+462);
-        line(i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+60, (min(width, height)/1.5+30)*(floor(i/floor(width/200)))+462,i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+70, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+472);
+        line(i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+70, (min(width, height)/1.5+30)*(floor(i/floor(width/200)))+472,i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+80, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+462);
+        line(i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+70, (min(width, height)/1.5+30)*(floor(i/floor(width/200)))+462,i%floor(width/200)*min(width, height)/1.5 + min(width,height)/4+80, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+472);
         fill(0, 0, 0);
         textSize(10);
-        text("Edit Constraints", min(width, height)/1.5*(i%floor(width/200)) + min(width,height)/4-20, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+467);
+        text("Edit Constraints", min(width, height)/1.5*(i%floor(width/200)) + min(width,height)/4, (min(width, height)/1.5 + 30)*(floor(i/floor(width/200)))+467);
         textAlign(LEFT, BASELINE);
         textSize(13);
         resetMatrix();
